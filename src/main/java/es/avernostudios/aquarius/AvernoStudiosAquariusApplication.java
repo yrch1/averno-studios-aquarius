de5db79fcdce2cbc21bf07e531c22262cc8f3f3b
dortega <dortega@dinersclub.es>
2016-04-14 08:43:39 +0200

    Initial commit

---
+++ src/main/java/es/avernostudios/aquarius/AvernoStudiosAquariusApplication.java
@@ -0,0 +1,12 @@
+package es.avernostudios.aquarius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvernoStudiosAquariusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvernoStudiosAquariusApplication.class, args);
+	}
+}
