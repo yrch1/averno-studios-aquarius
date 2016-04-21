045c878841a5b75a699ea67c9da4bbed741a80c0
Yrch <davidortegaingeniero@gmail.com>
2016-04-21 15:29:31 +0200

    Mejoras

--- src/main/java/es/avernostudios/aquarius/controllers/ProductsController.java
+++ src/main/java/es/avernostudios/aquarius/controllers/ProductsController.java
@@ -1,10 +1,26 @@
 package es.avernostudios.aquarius.controllers;
 
+import com.google.common.collect.Lists;
+import es.avernostudios.aquarius.Constants;
+import es.avernostudios.aquarius.bean.Product;
+import es.avernostudios.aquarius.jpa.repositories.ProductRepository;
+import es.avernostudios.aquarius.soap.helper.ProductInfoHelper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by dortega on 14/04/2016.
@@ -14,14 +30,74 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/products")
 public class ProductsController {
 
+    @Autowired
+    ProductRepository productRepository;
 
     @RequestMapping
-    public ModelAndView index(){
+    public ModelAndView index(@RequestParam(value = "storeId", required = false, defaultValue = "-1") int storeId
+            , @RequestParam(value = "offset", required = false, defaultValue = "1") int offset
+            , @RequestParam(value = "pageSize", required = false, defaultValue = Constants.PAGE_SIZE + "") int pageSize
+            , @RequestParam(value = "sortField", required = false, defaultValue = "sku") String sortField
+            , @RequestParam(value = "ascDesc", required = false, defaultValue = "ASC") String ascDesc
+            , @RequestParam(value = "q", required = false, defaultValue = "") String q
+            , HttpSession session) {
+
         LOGGER.debug("/products/");
         ModelAndView result = new ModelAndView();
 
+        int numPages;
+        int currentPage;
+        try {
+
+            if (storeId != -1 || session.getAttribute(Constants.STORE_ID) == null) {
+                session.setAttribute(Constants.STORE_ID, storeId);
+            }
+            List<Product> productsInfoList;
+            if (!q.equals("")) {
+
+                productsInfoList = new ArrayList<>();
+                productsInfoList.add(productRepository.findBySku(q));
+
+                numPages = 1;
+                currentPage = 1;
+
+            } else {
+
+
+                Pageable pageable = new PageRequest(offset-1,pageSize, Sort.Direction.fromStringOrNull(ascDesc),sortField);
+                Page<Product> page = productRepository.getInfoList(pageable);
+                productsInfoList = page.getContent();
+
+                numPages = page.getTotalPages();
+                currentPage = page.getNumber() + 1;
+
+            }
+
+            result.addObject("productList", productsInfoList);
+            result.addObject("numPages", numPages);
+            result.addObject("currentPage", currentPage);
+            result.addObject("pageSize", pageSize);
+
+
+        } catch (Exception e) {
+            LOGGER.error("Exception", e);
+        }
+
+
         result.setViewName("products/index");
         return result;
+    }
+
+
+    @RequestMapping(value="/{sku}", method = RequestMethod.GET)
+    public ModelAndView product(@PathVariable String sku
+            , HttpSession session) {
+
+        LOGGER.debug("/products/");
+        ModelAndView result = new ModelAndView();
+
+        result.setViewName("products/product");
+        return  result;
     }
 
 }
--- src/main/java/es/avernostudios/aquarius/jpa/repositories/ProductRepository.java
+++ src/main/java/es/avernostudios/aquarius/jpa/repositories/ProductRepository.java
@@ -1,6 +1,8 @@
 package es.avernostudios.aquarius.jpa.repositories;
 
 import es.avernostudios.aquarius.bean.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
@@ -18,4 +20,7 @@ public interface ProductRepository extends CrudRepository<Product, Integer> {
 
     @Query("select p from Product p where sku = ?1")
     Product findBySku(String additional);
+
+    @Query("select  p from Product p")
+    Page<Product> getInfoList(Pageable pageable);
 }
